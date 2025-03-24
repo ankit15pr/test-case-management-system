bbdbdbf9d2a6c71b5d0a82e53b894fae1efe4788
Ankit <ankitparmarranila@gmail.com>
2025-03-24 17:35:01 +0530

    Add pagination and filter in testcase repo

--- src/main/java/com/example/demo/Repository/TestCaseRepository.java
+++ src/main/java/com/example/demo/Repository/TestCaseRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.Repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.model.Priority;
@@ -9,5 +8,9 @@ import com.example.demo.model.Status;
 import com.example.demo.model.TestCase;
 
 public interface TestCaseRepository extends MongoRepository<TestCase, String> {
-    List<TestCase> findByStatusAndPriority(Status stutus, Priority priority);
+    Page<TestCase> findByStatus(Status status, org.springframework.data.domain.Pageable pageable);
+    
+    Page<TestCase> findByPriority(Priority priority, org.springframework.data.domain.Pageable pageable);
+
+    Page<TestCase> findByStatusAndPriority(Status status, Priority priority, org.springframework.data.domain.Pageable pageable);
 }
